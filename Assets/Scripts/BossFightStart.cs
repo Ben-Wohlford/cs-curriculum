3db289af1b123c1998d1445cefac43f5a0128f11
Ben-Wohlford <144173314+Ben-Wohlford@users.noreply.github.com>
2024-02-20 13:50:26 -0800

    02/20/2024

--- Assets/Scripts/BossFightStart.cs
+++ Assets/Scripts/BossFightStart.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class BossFightStart : MonoBehaviour
 {
+    [SerializeField] private GameObject bossWall;
+    private bool startLoop;
+    private int iterationCount;
+    private bool canStartLoop;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canStartLoop = true;
+        iterationCount = 3;
+        startLoop = false;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (startLoop)
+        {
+            for (int i = 0; i < iterationCount; i++)
+            {
+                if (i == iterationCount)
+                {
+                    startLoop = false;
+                    break;
+                }
+                Debug.Log(i);
+            }
+        }
     }
-
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && canStartLoop)
         {
+            canStartLoop = false;
             Debug.Log("boss");
+            startLoop = true;
         }
     }
 }
